d7497f4f7706d2b786e7d02609783d62f7610a49
MarkYegon7 <mark.yegon@student.moringaschool.com>
2022-05-23 01:10:35 +0300

    ft:intro file

--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class Config:
 
     EMAIL = "tomwere9@gmail.com"
     PASSWORD = "tom97"
+    MAIL_USE_SSL = True
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True 
+    MAIL_USERNAME = "tomwere9@gmail.com"
+    MAIL_PASSWORD ="svsss"
   
 class TestConfig(Config):
     pass
@@ -20,12 +26,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    MAIL_USE_SSL = True
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True 
-    MAIL_USERNAME = "tomwere9@gmail.com"
-    MAIL_PASSWORD ="svsss"
+    
 
     SQLALCHEMY_DATABASE_URI = 'postgresql://zgasjjhfqlsucn:1e3faa258e9d83eb6952ae9a90a5792812e2c22f5ff8b112d9ef4ea1bc39e521@ec2-54-165-184-219.compute-1.amazonaws.com:5432/dfe0f4c4hgqn5h'
     
